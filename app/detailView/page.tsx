08af636eb603e1964be033cd460c6400842dab4b
Tsiniloiv <jon-gisli93@hotmail.com>
2026-03-10 14:07:34 +0000

    Suspense Boundary

--- app/detailView/page.tsx
+++ app/detailView/page.tsx
@@ -4,6 +4,7 @@ import MainPost from "./mainPost";
 import CommentsDisplay from "./commentsDisplay";
 import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
+import {Suspense} from "react";
 
 /* 
 	Display post body and responses.
@@ -25,12 +26,14 @@ export default function Page() {
 	const { data, isLoading } = useSWR(link, getPostDetails)
 	if (isLoading || !data) return (<div>Loading!</div>);
 	return(
-		<div className="justify-items-center">
-			<main className="w-1/2">
-				<MainPost post={data.mainPost}/>
-				<span className="flex font-bold border-b-1">Comments</span>
-				<CommentsDisplay comments={data.comments} />
-			</main>
-		</div>
+		<Suspense fallback={<div>Loading!</div>}>
+			<div className="justify-items-center">
+				<main className="w-1/2">
+					<MainPost post={data.mainPost}/>
+					<span className="flex font-bold border-b-1">Comments</span>
+					<CommentsDisplay comments={data.comments} />
+				</main>
+			</div>
+		</Suspense>
 	)
 }
