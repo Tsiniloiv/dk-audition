084aea161c429102ef6391218dba38ecd4b9b19d
Tsiniloiv <jon-gisli93@hotmail.com>
2026-03-10 13:30:11 +0000

    Reverting some stuff for testing

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then(res => res.json());
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR("/api/reddit?path=pathofexile", fetcher);
+  const { data, error, isLoading } = useSWR("https://www.reddit.com/r/pathofexile.json", fetcher);
   if(error) {
     console.log(error);
     return(<div>Failed to fetch post data</div>);
